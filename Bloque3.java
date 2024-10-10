1598c6427c4ba56248b99171e0b9ad88e50d7431
Houyam <h.oubdil@colegiojosefinos.com>
2024-10-10 18:06:30 +0200

    Bloque3.java

--- Bloque3.java
+++ Bloque3.java
@@ -6,10 +6,6 @@ package com.mycompany.bloque4;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
